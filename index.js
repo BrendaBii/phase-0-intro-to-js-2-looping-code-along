afecd497380b7efecad148e4efc10982551d655b
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-08 15:30:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+//for loop
+const guestNames = ["Ada", "Brendan", "Ali"];
+
+function writeCards(guestNames,  eventName) {
+    const cards = [];
+  for (let i = 0; i < guestNames.length; i++) {
+    cards[i] = (`Thank you, ${guestNames[i]}, for the wonderful ${eventName} gift!`);
+    debugger;
+  }
+  return cards;
+}
+console. log (writeCards(guestNames, "birthday"));
+
+//while loop
+function countDown(count){
+    while (count >= 0) {
+      console.log(count--);
+    };
+    return count;
+}
+countDown("11");
